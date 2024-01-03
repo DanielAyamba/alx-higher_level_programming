fc59b423e36cd8fe3a580845da940c245480876b
DanielAyamba <danielayamba05@gmail.com>
2024-01-03 23:29:36 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-  print("{:c}".format(i), end="")
-    if i % 2 == 1:
-      i -= 32
+    print("{:c}".format(i), end="")
+        if i % 2 == 1:
+            i -= 32
